3e03ac294739be0aaecf0e2a89269e9cd964b99b
moonzpwr <60944420+moonzpwr@users.noreply.github.com>
2021-01-21 16:29:47 +0200

    add localStorege using

--- src/App.js
+++ src/App.js
@@ -9,14 +9,23 @@ import './App.css'
 
 class App extends Component {
 state = {
-  contacts: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'}
-  ],
+  contacts: [],
     filter: '',
 }
+  
+  componentDidUpdate( PrevState) { 
+    if (PrevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  }
+
+  componentDidMount() {
+    const localStoregeContacts = localStorage.getItem('contacts')
+    if (localStoregeContacts) {
+      this.setState({contacts: JSON.parse(localStoregeContacts)})
+    }
+    
+  }
   
   addContact = ({name, number}) => { 
     const contact = {
